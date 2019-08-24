c1ac0a97c1b97514d25bf395a6130b7e4ed7d27e
MaxXSoft <x@MaxXSoft.net>
2019-08-24 20:40:30 +0800

    (util) added more helper functions

--- view/script/util.js
+++ view/script/util.js
@@ -2,27 +2,64 @@
 const getWordCount = (text) => {
   let chinese = text.match(/[\u4e00-\u9fa5]/g)
   let english = text.match(/\b\w+\b/g)
-  let chineseLen = chinese != null ? chinese.length : 0;
-  let englishLen = english != null ? english.length : 0;
+  let chineseLen = chinese != null ? chinese.length : 0
+  let englishLen = english != null ? english.length : 0
   return chineseLen + englishLen
+}
+
+// fetch JSON from URL asynchronously
+const fetchJsonAsync = async (url) => {
+  let resp = await fetch(url)
+  return await resp.json()
 }
 
 // check if window is already scrolled to bottom
 const checkScroll = (callback, limit = 0) => {
+  let isCalled = false
   return () => {
     // calculate offset
     let d = document.documentElement
     let offset = d.scrollTop + window.innerHeight
     let height = d.offsetHeight
-    if (offset >= height - limit) callback()
+    if (!isCalled && offset >= height - limit) {
+      callback()
+      isCalled = true
+    }
+    else if (isCalled && offset < height - limit) {
+      isCalled = false
+    }
   }
+}
+
+// move branch panels in panel container
+// returns true if need to load more branch info
+const moveBranchPanel = (isLeft) => {
+  // get target element
+  let container = document.getElementById('panel-container')
+  let panel = document.querySelectorAll('.post-branch .panel')
+  if (!panel.length) return false
+  // calculate new left position
+  let left = container.offsetLeft
+  let offset = container.offsetWidth / panel.length
+  left += offset * (isLeft ? 1 : -1)
+  // check if the boundary is reached
+  let ret = false
+  if (left > 0) {
+    left = 0
+  }
+  else if (left <= -offset * (panel.length - 1)) {
+    left = -offset * (panel.length - 1)
+    ret = true
+  }
+  // set left position
+  container.style.left = left + 'px'
+  return ret
 }
 
 // get path info from server
 const getPathInfo = async (vm) => {
   // send request
-  let resp = await fetch(`${apiUrl}path/${postId}`)
-  let json = await resp.json()
+  let json = await fetchJsonAsync(`${apiUrl}path/${postId}`)
   // get path info from json
   if (!json.error) vm.path = json.response
   // check if need to reduce
@@ -37,23 +74,52 @@ const getPathInfo = async (vm) => {
 // get post info from server
 const getPostInfo = async (vm) => {
   if (fromRoot) {
+    const numberOfStories = 10
     // get post info after breakpoint
-    //
+    for (const i of vm.path.slice(vm.posts.length, numberOfStories)) {
+      let json = await fetchJsonAsync(`${apiUrl}info/${i}`)
+      vm.posts.push(!json.error ? json.response : null)
+    }
   }
   else {
+    vm.posts = []
     // get post info just by path
-    //
+    for (const i of vm.path) {
+      if (i < 0) {
+        // generate an ellipsis node
+        vm.posts.push({id: null, count: -i})
+      }
+      else {
+        // fetch post info
+        let json = await fetchJsonAsync(`${apiUrl}info/${i}`)
+        vm.posts.push(!json.error ? json.response : null)
+      }
+    }
   }
-}
-
-// load more posts when scroll to bottom
-const loadMorePosts = async () => {
-  //
 }
 
 // get branch info from server
 const getBranchInfo = async (vm) => {
-  //
+  // get branch count
+  let json = await fetchJsonAsync(`${apiUrl}childcount/${postId}`)
+  vm.branchCount = !json.error ? json.response : 0
+  // get initial branch info
+  getMoreBranchInfo(vm)
+}
+
+// load more branch info
+const getMoreBranchInfo = async (vm) => {
+  const numberOfBranches = 10
+  // get branch info after breakpoint
+  let start = vm.branches.length
+  if (start < vm.branchCount) {
+    // fetch branch info
+    let url = `${apiUrl}child/${postId}/${start}/${numberOfBranches}`
+    let json = await fetchJsonAsync(url)
+    if (!json.error) {
+      vm.branches = vm.branches.concat(json.response)
+    }
+  }
 }
 
 // get comment info from server
