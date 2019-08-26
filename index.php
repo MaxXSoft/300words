108732d38d059c45a790914f62478590a87365fa
MaxXSoft <x@MaxXSoft.net>
2019-08-26 22:57:17 +0800

    (index) modified some details

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 require_once 'config.php';
 require_once 'module/router.php';
 require_once 'view/random.php';
+require_once 'view/list.php';
+require_once 'view/about.php';
+require_once 'view/write.php';
+require_once 'view/story.php';
 require_once 'view/api.php';
 require_once 'view/post.php';
 
@@ -11,12 +15,12 @@ require_once 'view/post.php';
 Router::init('/300words', array(
   ''        => function () { return new RandomPageView(); },
   'random'  => function () { return new RandomPageView(); },
-  // 'hot'     => function () { return new HotPageView(); },
-  // 'latest'  => function () { return new LatestPageView(); },
-  // 'root'    => function () { return new RootPageView(); },
-  // 'about'   => function () { return new AboutPageView(); },
-  // 'new'     => function () { return new WritePageView(); },
-  // 'story'   => function () { return new StoryPageView(); },
+  'hot'     => function () { return new ListPageView(1); },
+  'latest'  => function () { return new ListPageView(2); },
+  'root'    => function () { return new ListPageView(3); },
+  'about'   => function () { return new AboutPageView(); },
+  'new'     => function () { return new WritePageView(); },
+  'story'   => function () { return new StoryPageView(); },
   'api'     => function () { return new APIView(); },
   'post'    => function () { return new PostView(); },
 ));
