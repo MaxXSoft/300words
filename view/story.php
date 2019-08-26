7dcdd2ccc1af0c067de5f7e5d2d990da9e534ba9
MaxXSoft <x@MaxXSoft.net>
2019-08-26 19:17:51 +0800

    (story) modified some details

--- view/story.php
+++ view/story.php
@@ -4,14 +4,23 @@ require_once 'page.php';
 
 class StoryPageView extends PageView {
 
+  // database object
+  private $dbo = null;
+
   public function __construct() {
     parent::__construct();
+    $this->using('dbo.php');
+    $this->dbo = new DBObject();
     $this->setCurrentTab(0);
   }
 
   public function render($args) {
+    $id = (int)$args[0];
+    $info = $this->dbo->getPostInfo($id);
+    $title = preg_split('/[\r\n]+/', trim($info['post']))[0];
     // initialize parameters
-    $this->setPostId((int)$args[0]);
+    $this->setTitle($title);
+    $this->setPostId($id);
     $this->setFromRoot($args[1] == 'fromroot');
     // render page
     $this->begin();
