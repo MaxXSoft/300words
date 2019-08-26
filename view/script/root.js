a9044ed0320681fe611d86da4f970eb0af9659d6
MaxXSoft <x@MaxXSoft.net>
2019-08-26 21:06:02 +0800

    (root) added more implementations for 'new story' page

--- view/script/root.js
+++ view/script/root.js
@@ -2,8 +2,18 @@
 const vmMain = new Vue({
   el: '#main-area',
   data: {
+    // post info
     totalCount: 0,
     posts: [],
+    // write area
+    content: '',
+    username: '',
+    remember: false,
+  },
+  computed: {
+    restLength: function () {
+      return 300 - getWordCount(this.content)
+    },
   },
   methods: {
     splitTextToLines: function (text) {
@@ -15,13 +25,24 @@ const vmMain = new Vue({
     getDiff: function (date) {
       return getDateDiff(date)
     },
+    submitPost: function () {
+      createNewStory(this, true)
+    },
   },
   created: async function () {
+    // initialize scroll detector
     window.onscroll = checkScroll(async () => {
       if (this.posts.length < this.totalCount) {
         getRootListInfo(this)
       }
     }, 100)
+    // get username from cookie
+    let username = getCookie('300words_username')
+    if (username) {
+      this.username = username
+      this.remember = true
+    }
+    // get info from server
     await getRootListCount(this)
     getRootListInfo(this)
   },
