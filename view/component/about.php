e97df2dcaea5286f740ce08bdc2609e73214a9e3
MaxXSoft <x@MaxXSoft.net>
2019-08-27 13:38:26 +0800

    (about) added component 'about.php'

---
+++ view/component/about.php
@@ -0,0 +1,90 @@
+<div class="post-box">
+  <div class="post-content even">
+    <h1>关于这个网站</h1>
+    <p>因为网站作者 MaxXing 很喜欢写故事，但是他的创作能力又十分有限，所以他想借助大家的力量，一起完成许许多多有趣的故事。</p>
+    <p>仔细想想，一群人写一个故事好像也挺有意思的。如果你看过 bilibili 曾经推出的一档名为《故事王》的综艺节目的话…… <del>（广告嫌疑微存？）</del></p>
+    <p>于是这个网站就诞生了。</p>
+  </div>
+  <div class="post-content odd">
+    <h1>什么是<i>故事接龙</i>？</h1>
+    <p>顾名思义，故事接龙类似于成语接龙。当一个人写出一篇短故事之后，其他人可以在此基础上接着讲另一个故事。以此类推，形成一个逐渐变长的故事串。</p>
+    <p>接龙的起点叫做根故事（root story），每个故事可以拥有多个接龙而成的子分支，最终接龙而成的故事会组成一棵以根故事为根节点的故事树（story tree）。</p>
+    <p>此网站允许用户发布或者接龙 300 字以内的短故事，所以得名“300字”。</p>
+  </div>
+  <div class="post-content even">
+    <h1>免责声明</h1>
+    <p><i>此部分的内容并未最终确定，您目前看到的是免责声明的 0.1 版本。</i></p>
+    <p><i>网站默认所有用户同意以下的免责声明。在网站实现用户注册功能后，注册时会要求用户阅读并同意该声明。</i></p>
+    <p><br></p>
+    <h2>无保证</h2>
+    <p>本网站按“原有状况”展现，无明示或暗示的陈述或保证。针对本网站或网站上的信息和资料，<?php $this->org(); ?> 不会提供任何陈述或保证。</p>
+    <p>在不影响上述通用性原则的前提下，<?php $this->org(); ?> 不提供以下保证：</p>
+    <ul>
+      <li>大部分时间或始终可以访问本网站；或者</li>
+      <li>本网站上的信息完整、真实、准确或不存在误导性。</li>
+    </ul>
+    <p>本网站上的任何内容，都不应当作为或理解为任何性质的建议。如果您希望得到有关问题的建议，您应咨询相关专业人员。</p>
+    <p><br></p>
+    <h2>责任限制</h2>
+    <p>针对本网站内容、使用或与本网站有关的以下情况，<?php $this->org(); ?> 不需要向您承担任何责任（无论合同法、侵权行为法等是否有规定）：</p>
+    <ul>
+      <li>本网站上任何免费提供信息导致的任何直接损失；</li>
+      <li>任何间接、特殊或从属损失；或者</li>
+      <li>业务损失、收入损失、收入、利润或预期节省，合同或业务关系、名誉或声誉、信息或数据丢失损失。</li>
+    </ul>
+    <p>即使明确告知 <?php $this->org(); ?> 可能造成损失，也应适用这些责任限制规定。</p>
+    <p><br></p>
+    <h2>隐私权保护</h2>
+    <p>本网站尊重并保护所有用户的个人隐私权，您注册的用户名、电子邮件地址等个人资料，非经您亲自许可或根据相关法律、法规的强制性规定，本网站不会主动地泄露给第三方。</p>
+    <p><br></p>
+    <h2>著作权保护</h2>
+    <p>根据著作权法：</p>
+    <ol>
+      <li>创作出的故事属于合作作品，可以分割使用的，由各个创作人对其创作的部分独自享有著作权；</li>
+      <li>不可以分割使用的，由各创作人协商一致决定其使用；</li>
+      <li>不能达成一致意见的，创作人不得阻止其他创作人对作品的使用，但是使用作品所得利益须与其他著作权人分享。</li>
+    </ol>
+    <p><br></p>
+    <h2>除外事项</h2>
+    <p>本网站免责声明中的任何内容，都不会排除或限制依法不能排除或限制的保证；而且也不能排除或限制 <?php $this->org(); ?> 的以下责任：</p>
+    <ul>
+      <li>因 <?php $this->org(); ?> 疏忽造成的死伤事故；</li>
+      <li><?php $this->org(); ?> 的欺诈行为或欺诈性陈述；或者</li>
+      <li><?php $this->org(); ?> 排除或限制，或者尝试或试图排除或限制其责任的行为，属于违法或非法的情况。</li>
+    </ul>
+    <p><br></p>
+    <h2>合理性</h2>
+    <p>使用本网站时，即可合理地视为您认可了本网站免责声明中的除外事项和责任限制。</p>
+    <p>如果您认为这点不合理，请不要使用本网站。</p>
+    <p><br></p>
+    <h2>第三方</h2>
+    <p>您认为，<?php $this->org(); ?> 会限制管理人员的个人责任。而且您同意，对于您因本网站遭受的任何损失，不会向 <?php $this->org(); ?> 的管理人员提出任何索赔。</p>
+    <p>在不影响上一条规定的前提下，您同意，本网站免责声明中的保证和责任限制，能够保护 <?php $this->org(); ?> 管理人员及 <?php $this->org(); ?> 本身的权益。</p>
+    <p><br></p>
+    <h2>无法执行</h2>
+    <p>如果依据相关法律本网站免责声明中的某个条款无法执行，这不会影响本网站免责声明中其他条款的可执行性。</p>
+    <p><br></p>
+    <h2>感谢</h2>
+    <p>本声明参考了 <a href="http://www.freenetlaw.com" target="_blank"><b>Contractology</b></a> 的模板。</p>
+  </div>
+  <div class="post-content odd">
+    <!-- NOTE: If you try to edit this about page, you must keep this part of content. -->
+    <h1>依赖的开源框架/组件</h1>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue" target="_blank"><b>Vue.js</b></a> By Evan You</li>
+      <li><a href="https://github.com/Akryum/v-tooltip" target="_blank"><b>v-tooltip</b></a> By Akryum</li>
+      <li><a href="https://github.com/devstark-com/vue-textarea-autosize" target="_blank"><b>vue-textarea-autosize</b></a> By Devstark</li>
+    </ul>
+    <p>开源软件的兴盛前所未有地推动了计算机行业的发展，在此感谢所有为开源软件努力和付出的开发者们。</p>
+    <p>此网站同样开源，并且遵循 MIT 许可证，详情请查看 <a href="https://github.com/MaxXSoft/300words" target="_blank"><b>300words@GitHub</b></a>。</p>
+  </div>
+  <div class="post-content even">
+    <h1><a id="coop"></a>合作</h1>
+    <p>300字（300words）项目目前由 MaxXing 个人维护。</p>
+    <p>如果您在使用网站的过程中发现了 bug，或者对网站有任何意见或建议，您可以在该项目的 GitHub 仓库中<a href="https://github.com/MaxXSoft/300words/issues" target="_blank"><b>提交 issue</b></a>，或者邮件联系 <a href="mailto:x@MaxXSoft.net"><b>MaxXing 本人</b></a>。</p>
+    <p>同样，如果您是一位对此项目感兴趣的开发者，并且希望参与项目的维护过程，您也可以在 <a href="https://github.com/MaxXSoft/300words" target="_blank"><b>GitHub 仓库</b></a>中提交 pull request。</p>
+  </div>
+  <div class="infobar">
+    <div class="username">By MaxXing, 2019-8-27</div>
+  </div>
+</div>
